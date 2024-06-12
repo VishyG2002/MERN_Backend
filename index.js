91400aa9096e0a0bb4d346927d6a074cc6a2e168
guptavishu1000 <guptavishu1000@gmail.com>
2024-06-12 22:44:57 +0530

    minor change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173' || 'https://books-store-webapp.netlify.app/',
+    origin: 'http://localhost:5173' || 'https://books-store-webapp.netlify.app',
     methods: ['GET','POST','PUT','DELETE'],
     allowedHeaders: ['Content-Type'],
   })

778ffbf5fb3537c1fb5bec4f174781fa0b927543
guptavishu1000 <guptavishu1000@gmail.com>
2024-06-12 20:41:56 +0530

    changed index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express, { response } from "express";
-// import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';

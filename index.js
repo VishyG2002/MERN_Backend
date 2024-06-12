bfdc672975869a781f5d65b45c7b2a8ce048e794
guptavishu1000 <guptavishu1000@gmail.com>
2024-06-12 22:50:57 +0530

    minor change related to cors

--- index.js
+++ index.js
@@ -8,13 +8,28 @@ const PORT = process.env.PORT || 5555
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'http://localhost:5173' || 'https://books-store-webapp.netlify.app',
-    methods: ['GET','POST','PUT','DELETE'],
+
+// Allowlist for allowed origins
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://books-store-webapp.netlify.app'
+  ];
+  
+  const corsOptions = {
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
-  })
-)
+  };
+  
+  app.use(cors(corsOptions));
 
 app.get('/', (reqest,response) =>{
     console.log(reqest);
